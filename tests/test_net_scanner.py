9a744ee3b8e9a0642c3b948d0662b928e8ced9db
afcsi <a.ferreira@formation-cci12.fr>
2025-04-04 11:15:15 +0200

    Ajout de test avec unittest

--- tests/test_net_scanner.py
+++ tests/test_net_scanner.py
@@ -1,1 +1,10 @@
+import unittest
+from src.net_scanner import ping_ip
 
+class TestNetScanner(unittest.TestCase):
+    def test_ping_ip(self):
+        result = ping_ip("127.0.0.1")
+        self.assertEqual(result[1], "Active")
+
+if __name__ == "__main__":
+    unittest.main()
